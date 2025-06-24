f4b7e176f49724266847f54545de96f3d01f6ed3
study-path <liliyaingul@gmail.com>
2025-06-24 19:01:35 -0400

    Two sum - (1)

---
+++ Two sum - (1)/index.js
@@ -0,0 +1,14 @@
+
+  @param {number[]} nums
+  @param {number} target
+  @return {number[]}
+ 
+var twoSum = function(nums, target) {   
+    for(let i = 0; i  nums.length; i++){
+        for(let y = i+1; y  nums.length;y++){
+            if(nums[i] + nums[y] === target){
+                return [i, y]           
+             }
+        }
+    }
+};
