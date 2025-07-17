4fe28f849889dcf4ae141a2c53e67b04fd07fd86
study-path <liliyaingul@gmail.com>
2025-07-17 16:31:09 -0400

    Refactoring Solution for 263.Ugly Number

--- Algorithms/Ugly Number - (263)/index.js
+++ Algorithms/Ugly Number - (263)/index.js
@@ -3,14 +3,17 @@
  * @return {boolean}
  */
 var isUgly = function(n) {
-    while (n % 2 === 0) {
-        n = n / 2;
+    if(n>0){
+        while (n % 2 === 0) {
+            n = n / 2;
+        }
+        while (n % 3 === 0) {
+            n = n / 3;
+        }
+        while (n % 5 === 0) {
+            n = n / 5;
+        }
+        return n === 1;
     }
-    while (n % 3 === 0) {
-        n = n / 3;
-    }
-    while (n % 5 === 0) {
-        n = n / 5;
-    }
-    return n === 1;
+    else return false
 };
