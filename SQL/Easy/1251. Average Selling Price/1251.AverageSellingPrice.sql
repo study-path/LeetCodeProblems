a34691568789d0ef9c0c6409b442d20390e03e6e
study-path <liliyaingul@gmail.com>
2025-09-29 16:41:48 -0400

    edit file

--- SQL/Easy/1251. Average Selling Price/1251.AverageSellingPrice.sql
+++ SQL/Easy/1251. Average Selling Price/1251.AverageSellingPrice.sql
@@ -1,5 +1,4 @@
-
--- # Problem: https://leetcode.com/problems/average-selling-price/submissions/1776243563/
+-- # Problem: https://leetcode.com/problems/average-selling-price/
 
 select product_id, isnull(round(sum(average_price) * 1.0 / sum(units), 2), 0) as average_price
 from (
@@ -8,8 +7,3 @@ from (
  right join Prices as p on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
 ) a
 group by product_id
- 
-
-
-
- 
