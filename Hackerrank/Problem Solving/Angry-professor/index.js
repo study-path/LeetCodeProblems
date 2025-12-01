c2533527dadcba4e615349a1fbab5a3d12a220c8
study-path <liliyaingul@gmail.com>
2025-12-01 15:29:26 -0500

    Add angry-professor

---
+++ Hackerrank/Problem Solving/Angry-professor/index.js
@@ -0,0 +1,16 @@
+# Problem: https://www.hackerrank.com/challenges/angry-professor
+
+function angryProfessor(k, a) {
+    let count = 0;
+    for(let i = 0; i< a.length; i++){
+        if(a[i] <= 0 ){
+           count++; 
+        }
+    }
+    if(count < k){
+        return "YES"
+    }
+    else{
+        return "NO"
+    }
+}
